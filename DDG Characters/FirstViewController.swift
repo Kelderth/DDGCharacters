650c56caf0af564853ac956f2774e160196f61e9
Kelderth <Kelderth@gmail.com>
2018-12-18 16:24:23 -0500

    Initial Commit

---
+++ DDG Characters/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DDG Characters
+//
+//  Created by MCS on 12/18/18.
+//  Copyright © 2018 Kelderth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
